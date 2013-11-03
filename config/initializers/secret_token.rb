b0e210b8a972fec93358d24bc3891411c9279088
Benjamin Lee <benjamsandwich@gmail.com>
2013-11-03 14:57:42 -0800

    Create Rails project, modify Gemfile

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BriskPaper::Application.config.secret_token = '1fe23ba255c68242655ad21051cbc48e3cbb70c2bd2aebba191ab3148fe4cd84655ee48fd3da7485aa1532a60419bcff38fa2d9418e18d8ef2d6538dcd52eab3'
