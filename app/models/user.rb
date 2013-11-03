01479d99004c157841cad073c0214546f56ba2c0
Benjamin Lee <benjamsandwich@gmail.com>
2013-11-03 15:18:48 -0800

    Add session_token column to users table

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,25 @@ class User < ActiveRecord::Base
   validates :email, :password, presence: true
   validates :email, uniqueness: true
   
+  def generate_session_token
+    SecureRandom.urlsafe_base64(16)
+  end
   
+  def reset_session_token!
+    self.session_token = self.class.generate_session_token
+    self.save!
+  end
+  
+  def is_password?(password)
+    
+  end
+  
+  def self.find_by_credentials(email, password)
+    user = User.find_by_email(email)
+    if user
+      return user if user.is_password?(password)
+    end
+  end
   
   
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131103230110) do
+ActiveRecord::Schema.define(:version => 20131103231713) do
 
   create_table "users", :force => true do |t|
-    t.string   "email",      :null => false
-    t.string   "password",   :null => false
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.string   "email",         :null => false
+    t.string   "password",      :null => false
+    t.datetime "created_at",    :null => false
+    t.datetime "updated_at",    :null => false
+    t.string   "session_token"
   end
 
 end
