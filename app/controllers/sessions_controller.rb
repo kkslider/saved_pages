bd162a21fe404aef924ecea2c2111092aa020c38
Benjamin Lee <benjamsandwich@gmail.com>
2013-11-03 15:47:57 -0800

    Create SessionsController

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,9 +1,14 @@
 class SessionsController < ApplicationController
   def create
-    
+    user = User.find_by_credentials(
+      params[:user][:email],
+      params[:user][:password] 
+    )
   end
   
   def destroy
+    logout_current_user!
     
+    # need to redirect somewhere
   end
 end
