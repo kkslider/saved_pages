3877a7968dfd1112b3c9e437147a06eeb5b2414e
Benjamin Lee <benjamsandwich@gmail.com>
2013-11-03 17:47:15 -0800

    Create root page for app, create Users controller

--- app/assets/javascripts/brisk_paper.js
+++ app/assets/javascripts/brisk_paper.js
@@ -4,7 +4,7 @@ window.BriskPaper = {
   Views: {},
   Routers: {},
   initialize: function() {
-    alert('Hello from Backbone!');
+    // alert('Hello from Backbone!');
   }
 };
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 BriskPaper::Application.routes.draw do
   # resources :user
   get 'u', to: 'users#show'
+  
+  root :to => "root#root"
 end
