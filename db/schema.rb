a8c57b37d91194af69724a665b3f6a8dc4ff80a0
Benjamin Lee <benjamsandwich@gmail.com>
2013-11-04 01:33:50 -0800

    Add unique index on email in users table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131104055500) do
+ActiveRecord::Schema.define(:version => 20131104055624) do
 
   create_table "bookmarks", :force => true do |t|
     t.integer  "user_id",    :null => false
@@ -31,5 +31,7 @@ ActiveRecord::Schema.define(:version => 20131104055500) do
     t.string   "session_token"
     t.string   "password_digest", :null => false
   end
+
+  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
 
 end
