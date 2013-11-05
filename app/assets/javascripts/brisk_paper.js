178203888939aea084c35000864b4ba25f4e1884
Benjamin Lee <benjamsandwich@gmail.com>
2013-11-05 15:20:05 -0800

    Add div around first list item of #content, add border to divs to distinguish

--- app/assets/javascripts/brisk_paper.js
+++ app/assets/javascripts/brisk_paper.js
@@ -4,19 +4,17 @@ window.SavedPages = {
   Views: {},
   Routers: {},
   initialize: function() {
-    alert("nice!");
     SavedPages.bookmarks = new SavedPages.Collections.Bookmarks();
     
     SavedPages.bookmarks.fetch({
       success: function() {
-        alert("success!");
+        console.log("success!");
         new SavedPages.AppRouter({
           $rootEl: $("#content")
         });
         Backbone.history.start();
       }
     });
-    
   }
 };
 
