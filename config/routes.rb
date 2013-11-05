84b0eda39495396075e3b23b039f914a71cf2dfb
Benjamin Lee <benjamsandwich@gmail.com>
2013-11-04 21:19:33 -0800

    Create route for archive index, Archive controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ SavedPages::Application.routes.draw do
   get 'u', to: 'users#show'
   resources :users, :only => [:new, :create]
   resource :session, :only => [:new, :create]
+  resources :archive, :only => [:index]
   
   get 'liked', to: 'favorites#index'
   
