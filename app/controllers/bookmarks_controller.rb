e198dcff19cf82b5f59d575459d248e5d165d43c
Benjamin Lee <benjamsandwich@gmail.com>
2013-11-04 21:37:34 -0800

    Create new bookmark form

---
+++ app/controllers/bookmarks_controller.rb
@@ -0,0 +1,5 @@
+class BookmarksController < ApplicationController
+  def create
+    
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 SavedPages::Application.routes.draw do
-  # resources :user
   get 'u', to: 'users#show'
   resources :users, :only => [:new, :create]
+  
   resource :session, :only => [:new, :create]
+  
+  resources :bookmarks, :only => [:new, :create]
+  
   get 'archive', to: 'archives#index'
+  
   get 'liked', to: 'favorites#index'
   
   root :to => "root#root"
