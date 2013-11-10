ec2ca1f7addfa2121e4bbd11be6b2919d51930e3
Benjamin Lee <benjamsandwich@gmail.com>
2013-11-09 22:54:16 -0800

    Add bottom border for last row of bookmarks, default div for no bookmark views

--- app/assets/javascripts/views/bookmarks/archive.js
+++ app/assets/javascripts/views/bookmarks/archive.js
@@ -1,4 +1,12 @@
 SavedPages.Views.Archive = Backbone.View.extend({
+  events: {
+    "mouseover .bookmark_row": "mouseOverBookmark",
+    "mouseout .bookmark_row": "mouseOutBookmark",
+    "click #like_bookmark": "likeBookmark",
+    "click #archive_bookmark": "archiveBookmark",
+    "click #delete_bookmark": "deleteBookmark",
+  },
+  
   template: JST["bookmarks/archive"],
   
   render: function() {
@@ -8,5 +16,63 @@ SavedPages.Views.Archive = Backbone.View.extend({
     
     this.$el.html(renderedContent);
     return this;
+  },
+  
+  mouseOverBookmark: function(event) {
+    $(event.currentTarget).css('background-color', '#fafafa');
+    // $(event.currentTarget).find("a").css('color', '#ba0f06');
+    $("a", event.currentTarget).css('color', '#ba0f06');
+    $(".options", event.currentTarget).show();
+  },
+  
+  mouseOutBookmark: function(event) {
+    $(event.currentTarget).css('background-color', '#ffffff');
+    // $(event.currentTarget).find("a").css('color', '#000000');
+    $("a", event.currentTarget).css('color', '#000000');
+    $(".options", event.currentTarget).hide();
+  },
+  
+  likeBookmark: function(event) {
+    event.preventDefault();
+    var bookmarkId = $(event.currentTarget).attr("data-bookmark-id");
+    var bookmark = SavedPages.bookmarks.get(bookmarkId);
+    bookmark.like();
+    // $(event.currentTarget).toggle("puff");
+    var that = $(event.currentTarget);
+    $(event.currentTarget).effect({ effect: "puff", complete: function() {
+      that.fadeIn();
+    }});
+  },
+  
+  archiveBookmark: function(event) {
+    event.preventDefault();
+    var bookmarkId = $(event.currentTarget).attr("data-bookmark-id");
+    var bookmark = SavedPages.bookmarks.get(bookmarkId);
+    bookmark.archive_this();
+    
+    $(event.target).parent().parent().parent().parent().parent().parent().hide("slide", 
+      { direction: "left" });
+  },
+  
+  deleteBookmark: function(event) {
+    event.preventDefault();
+    var response = confirm("Are you sure you want to permanently delete this article?");
+    if (response) {
+      var bookmarkId = $(event.currentTarget).attr("data-bookmark-id");
+      var bookmark = SavedPages.bookmarks.get(bookmarkId);
+      alert(bookmarkId);
+      // alert(bookmark);
+      // bookmark.destroy({
+      //   success: function(model, response) {
+      //     
+      //     $(event.target).parent().parent().parent().parent().parent().parent().hide("slide", { direction: "up" });
+      //     Backbone.history.navigate("/", { trigger: true });
+      //   }
+      // });
+      bookmark.destroy();
+      $(event.target).parent().parent().parent().parent().parent().parent().hide("slide", { direction: "up" });
+    }
   }
+  
+  
 });
