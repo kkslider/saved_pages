e42f28f82d13d11bb5907a479484c9581e33db09
Benjamin Lee <benjamsandwich@gmail.com>
2013-11-11 20:46:35 -0800

    Configure for CORS

--- config/application.rb
+++ config/application.rb
@@ -60,7 +60,7 @@ module SavedPages
     config.assets.version = '1.0'
     config.assets.initialize_on_precompile = false
     
-    config.middleware.insert_before ActionDispatch::Static, Rack::Cors do
+    config.middleware.use Rack::Cors do
       allow do
         origins '*'
         resource '*', :headers => :any, :methods => [:get, :post, :options]
