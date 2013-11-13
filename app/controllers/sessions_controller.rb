dbb6209b972672599ac053d06f7f5b67de007787
Benjamin Lee <benjamsandwich@gmail.com>
2013-11-13 11:29:07 -0800

    Make create account/sign in link to Rails views

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,8 @@ class SessionsController < ApplicationController
     
     if user
       login_user!(user)
-      render :json => user
+      # render :json => user
+      redirect_to '#/u'
       # redirect/render
     else
       render :json => user.errors.full_messages, status: 422
