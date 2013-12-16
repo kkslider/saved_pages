41d1e436099195dddebdd89e275398e0a0b8285a
Benjamin Lee <benjamsandwich@gmail.com>
2013-12-15 22:40:05 -0800

    Add more User model tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,6 +4,13 @@ describe User do
   it "has a valid factory" do
     FactoryGirl.create(:user).should be_valid
   end
-  it "is invalid without an email"
-  it "is invalid without a password"
+  
+  it "is invalid without an email" do
+    FactoryGirl.build(:user, email: nil).should_not be_valid
+  end
+  
+  it "is invalid without a password" do
+    FactoryGirl.build(:user, password: nil).should_not be_valid
+  end
+  
 end
